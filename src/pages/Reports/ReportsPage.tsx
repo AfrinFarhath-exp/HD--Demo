76ba4cbb3332967f4aa1bdcc20fad4e448e63e20
AfrinFarhath-exp <afrin.farhath@experionglobal.com>
2025-05-15 12:53:31 +0530

    reports json added

--- src/pages/Reports/ReportsPage.tsx
+++ src/pages/Reports/ReportsPage.tsx
@@ -1,17 +1,14 @@
 import ReportCard from "../../components/ReportCard";
 
-const reports = [
-  "Backorder Weekly Report",
-  "eCommerce Team Revenue Report",
-  "Fraud Order Monthly Report",
-  "Returns Report",
-  "GWP Orders Report",
-  "SMGM Orders Report",
-];
+
+import reports from "../../data/report"; 
 
 export default function ReportsPage() {
+  const firstRow = reports.slice(0, 5);
+  const secondRow = reports.slice(5);
+
   return (
-    <div className="min-h-screen bg-[#f4f1ee] px-8 pt-32 pb-8"> 
+    <div className="min-h-screen bg-[#f4f1ee] px-8 pt-32 pb-8">
       <h1 className="text-4xl font-bold text-center mb-6">Reports</h1>
 
       <div className="flex justify-center mb-4">
@@ -22,14 +19,26 @@ export default function ReportsPage() {
         />
       </div>
 
-      
-
-      <div className="flex flex-wrap gap-4 justify-center pt-10">
-        {reports.map((title) => (
-          <ReportCard key={title} title={title} />
+     
+      <div className="flex flex-wrap justify-center gap-4 pt-10">
+        {firstRow.map((report) => (
+          <ReportCard key={report.title} title={report.title} />
         ))}
       </div>
-      <p className="text-center text-gray-500 mb-10 pt-10">Choose the reports you need...</p> 
+
+      
+      <div className="h-8" />
+
+     
+      <div className="flex flex-wrap justify-center gap-4">
+        {secondRow.map((report) => (
+          <ReportCard key={report.title} title={report.title} />
+        ))}
+      </div>
+
+      <p className="text-center text-gray-500 mb-10 pt-10">
+        Choose the reports you need...
+      </p>
     </div>
   );
 }
