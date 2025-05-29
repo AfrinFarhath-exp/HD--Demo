36fa22a6eff3c3006fd814c353f770f0756765e4
Ajin-k-j <ajinkjajin@gmail.com>
2025-05-29 12:15:46 +0530

    api chnaged

--- src/components/Report/azureSearchService.tsx
+++ src/components/Report/azureSearchService.tsx
@@ -19,7 +19,7 @@ interface AzureSearchResult {
 
 export const azureSearchService = {
   apiEndpoint:
-    "http://localhost:8000/ai_agent_query",
+    "https://hdsupportapi-hff8cqb9a8g2brar.canadacentral-01.azurewebsites.net/ai_agent_query",
 
   async searchReports(
     query: string,
